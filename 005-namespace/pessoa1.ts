d8444274535edaf63589c8759adcdb94ca143c69
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2024-08-27 20:03:13 -0300

    Create pessoa1.ts

---
+++ 005-namespace/pessoa1.ts
@@ -0,0 +1,11 @@
+// Namespace
+
+export namespace Pessoa1 {
+     export let nome: string = 'João';
+
+      export function calc(): number{
+        return 1 + 1;
+      }
+};
+
+console.log(Pessoa1.nome);
