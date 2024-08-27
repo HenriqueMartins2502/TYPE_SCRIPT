b27a1bd9f7641bcf95612f03959555c86fb58570
Henrique Martins <160790963+HenriqueMartins2502@users.noreply.github.com>
2024-08-26 21:20:19 -0300

    Create index.ts

---
+++ 001-introdução/index.ts
@@ -0,0 +1,10 @@
+// no terminal npm install -g typescript(para instalar o typescript)
+// no terminal tsc --init(para criar o json)
+// fazer rodar com cntrl + alt + n
+
+const pedido = (pedido: any) => {
+    return `deu bom o seu pedido ${pedido}`
+
+};
+
+console.log(pedido(123));
